e1d4944b66b5afb8a2e0bc2ace934dcad4be4e54
echissico <edmilson.chissico@cedsif.gov.mz>
2026-08-18 23:48:06 +0200

    ne2w

--- api/index.php
+++ api/index.php
@@ -2,6 +2,20 @@
 
 header('Content-Type: text/plain');
 
-echo "API INDEX OK\n";
-echo "PHP VERSION: " . PHP_VERSION . "\n";
-echo "TMP WRITABLE: " . (is_writable('/tmp') ? 'YES' : 'NO') . "\n";
+echo "1. START\n";
+
+require __DIR__ . '/../vendor/autoload.php';
+
+echo "2. AUTOLOAD OK\n";
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+echo "3. BOOTSTRAP OK\n";
+
+$app->register(\Illuminate\View\ViewServiceProvider::class);
+
+echo "4. VIEW PROVIDER OK\n";
+
+var_dump($app->bound('view'));
+
+echo "5. END\n";
