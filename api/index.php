82be21ca8db1c24c0a6033b8cf0afd09e0d859e9
echissico <edmilson.chissico@cedsif.gov.mz>
2026-08-18 23:58:00 +0200

    ne2w

--- api/index.php
+++ api/index.php
@@ -1,12 +1,36 @@
 <?php
 
-use Illuminate\Http\Request;
+header('Content-Type: text/plain');
+
+echo "1. START\n";
+flush();
 
 require __DIR__.'/../vendor/autoload.php';
 
+echo "2. AUTOLOAD OK\n";
+flush();
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
-echo $app->version();
+
+echo "3. BOOTSTRAP OK\n";
+flush();
 
 $app->register(\Illuminate\View\ViewServiceProvider::class);
 
-$app->handleRequest(Request::capture());
+echo "4. VIEW PROVIDER OK\n";
+flush();
+
+var_dump($app->bound('view'));
+
+echo "5. VIEW BOUND OK\n";
+flush();
+
+$request = \Illuminate\Http\Request::capture();
+
+echo "6. REQUEST OK\n";
+flush();
+
+$response = $app->handleRequest($request);
+
+echo "7. RESPONSE OK\n";
+flush();
