1ef9ba252147a7f6a8e044f09f44d2626397e5ee
echissico <edmilson.chissico@cedsif.gov.mz>
2026-08-18 23:44:23 +0200

    new

--- api/index.php
+++ api/index.php
@@ -1,13 +1,30 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+$tmp = '/tmp/laravel';
+
+foreach ([
+    $tmp,
+    $tmp.'/storage',
+    $tmp.'/storage/app',
+    $tmp.'/storage/framework',
+    $tmp.'/storage/framework/cache',
+    $tmp.'/storage/framework/sessions',
+    $tmp.'/storage/framework/views',
+    $tmp.'/storage/logs',
+] as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
 require __DIR__.'/../vendor/autoload.php';
 
-/** @var Application $app teste */
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->useStoragePath($tmp.'/storage');
 
 $app->handleRequest(Request::capture());
