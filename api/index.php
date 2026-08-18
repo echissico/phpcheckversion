8b4a8b179fee61a7f2cbaf18239396cc6c4c8f94
echissico <edmilson.chissico@cedsif.gov.mz>
2026-08-18 23:53:30 +0200

    ne2w

--- api/index.php
+++ api/index.php
@@ -1,31 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
+header('Content-Type: text/plain');
 
-define('LARAVEL_START', microtime(true));
-
-$tmp = '/tmp/laravel';
-
-foreach ([
-    $tmp,
-    $tmp.'/storage',
-    $tmp.'/storage/app',
-    $tmp.'/storage/framework',
-    $tmp.'/storage/framework/cache',
-    $tmp.'/storage/framework/sessions',
-    $tmp.'/storage/framework/views',
-    $tmp.'/storage/logs',
-] as $dir) {
-    if (!is_dir($dir)) {
-        mkdir($dir, 0755, true);
-    }
-}
-
-require __DIR__.'/../vendor/autoload.php';
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->useStoragePath($tmp.'/storage');
-
-$app->handleRequest(Request::capture());
+echo "DEPLOY OK\n";
+echo "PHP: " . PHP_VERSION . "\n";
+echo "TIME: " . date('c') . "\n";
