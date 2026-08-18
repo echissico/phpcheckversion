c4571b34690698105f66e5bced83a6c0a4b0829a
echissico <edmilson.chissico@cedsif.gov.mz>
2026-08-18 22:49:59 +0200

    teste2

--- api/index.php
+++ api/index.php
@@ -9,3 +9,7 @@ echo "AUTOLOAD OK<br>";
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 echo "BOOTSTRAP OK<br>";
+
+$request = Illuminate\Http\Request::capture();
+
+echo "REQUEST OK<br>";
