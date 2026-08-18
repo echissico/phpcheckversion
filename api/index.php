8280999c375068fc86e732810d97188738daf585
echissico <edmilson.chissico@cedsif.gov.mz>
2026-08-18 23:56:08 +0200

    ne2w

--- api/index.php
+++ api/index.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
+echo $app->version();
 
 $app->register(\Illuminate\View\ViewServiceProvider::class);
 
