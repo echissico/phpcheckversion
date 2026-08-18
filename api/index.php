aaf4d1eead040a6328e594c97924f832106d4b51
echissico <edmilson.chissico@cedsif.gov.mz>
2026-08-18 23:26:24 +0200

    teste provider

--- api/index.php
+++ api/index.php
@@ -4,12 +4,12 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-var_dump($app->bound('view'));
-
 var_dump(
     $app->getProviders(
         \Illuminate\View\ViewServiceProvider::class
     )
 );
+
+var_dump($app->bound('view'));
 
 exit;
