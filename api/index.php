f6ecba434b153f8647e68c9715254881b0aa2240
echissico <edmilson.chissico@cedsif.gov.mz>
2026-08-18 22:51:22 +0200

    teste3

--- api/index.php
+++ api/index.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 echo "API INDEX OK<br>";
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -13,3 +16,19 @@ echo "BOOTSTRAP OK<br>";
 $request = Illuminate\Http\Request::capture();
 
 echo "REQUEST OK<br>";
+
+try {
+    $app->handleRequest($request);
+
+    echo "HANDLE REQUEST OK<br>";
+} catch (\Throwable $e) {
+    echo "<h1>ERRO LARAVEL</h1>";
+    echo "<pre>";
+    echo "Classe: " . get_class($e) . PHP_EOL;
+    echo "Mensagem: " . $e->getMessage() . PHP_EOL;
+    echo "Arquivo: " . $e->getFile() . PHP_EOL;
+    echo "Linha: " . $e->getLine() . PHP_EOL;
+    echo PHP_EOL;
+    echo $e->getTraceAsString();
+    echo "</pre>";
+}
