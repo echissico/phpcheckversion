e0bc66df677d98e7213e680dc8dc728105665222
echissico <edmilson.chissico@cedsif.gov.mz>
2026-08-18 22:32:04 +0200

    fix index require

--- api/index.php
+++ api/index.php
@@ -1,13 +1,3 @@
 <?php
 
 require __DIR__ . '/../public/index.php';
-
-
-echo 'ola my testing the version of php';
-phpinfo();
-header('Content-Type: application/json');
-
-echo json_encode([
-    'mensagem' => 'Olá! PHP funcionando no Vercel.',
-    'versao_php' => PHP_VERSION
-]);
