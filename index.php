2d31b286041f3414152286e301a0038cd25346f7
Edmilson Chissico <52789750+echissico@users.noreply.github.com>
2026-08-18 20:24:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php
-echo PHP_VERSION;
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Bem-vindo</title>
+</head>
+<body>
+
+    <h1>Bem-vindo ao meu site!</h1>
+    <p>É um prazer ter você aqui.</p>
+
+</body>
+</html>
