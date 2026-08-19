43bb647138d9be75ac897f16f9470c1cbf3cc8b8
echissico <edmilson.chissico@cedsif.gov.mz>
2026-08-19 22:49:45 +0200

    nw try

--- api/index.php
+++ api/index.php
@@ -26,6 +26,12 @@ foreach ($directories as $directory) {
     }
 }
 
+$_ENV['APP_SERVICES_CACHE'] = $tmp.'/storage/bootstrap/cache/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = $tmp.'/storage/bootstrap/cache/packages.php';
+$_ENV['APP_CONFIG_CACHE'] = $tmp.'/storage/bootstrap/cache/config.php';
+$_ENV['APP_ROUTES_CACHE'] = $tmp.'/storage/bootstrap/cache/routes.php';
+$_ENV['APP_EVENTS_CACHE'] = $tmp.'/storage/bootstrap/cache/events.php';
+
 require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
@@ -35,4 +41,5 @@ $app->useStoragePath($tmp.'/storage');
 $app->register(\Illuminate\View\ViewServiceProvider::class);
 
 $app->handleRequest(Request::capture());
+
 
